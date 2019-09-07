2eadc5215c71d3960374f5ca392a349a025d1452
walkdeadtobe <2017hemu@gmail.com>
2019-09-07 18:46:15 +0800

    #

--- 8_字符串转换整数/Solution.java
+++ 8_字符串转换整数/Solution.java
@@ -8,27 +8,51 @@ class Solution {
             //两种类型字符,空格和非空格
             //对于空格,统一跳过
             //以正负号作为数字的 开头,可能出现正负号之后有字符然后是数字的情况(ps:从题目来看,忽略这一点)
-            if(str.charAt(i).equals(' ')||(start!=-1&&(int)(str.charAt(i))>=48&&(int)(str.charAt(i))<=48))
-                continue;
+            //if(str.charAt(i).equals(' ')||(start!=-1&&(int)(str.charAt(i))>=48&&(int)(str.charAt(i))<=48))
+            //    continue;
             if(start==-1)
             {
-                if(str.charAt(i).equals('+')||str.charAt(i).equals('-')||((int)(str.charAt(i))>=48&&(int)(str.charAt(i))<=48)))
+                if((int)(str.charAt(i))==(int)('+')||(int)(str.charAt(i))==(int)('-')||((int)(str.charAt(i))>=48&&(int)(str.charAt(i))<=57))
                 {
                     start=i;
-                    if(str.charAt(i).equals('-'))
-                        positive=flase;
+                    if((int)(str.charAt(i))==(int)('-'))
+                        positive=false;
                 }
                 else
                 {
-                    
+                    return 0;
                 }
             }
             else
             {
+                if((int)(str.charAt(i))>=48&&(int)(str.charAt(i))<=57)
+                    continue;
+                else
+                    {
+                        end=i;
+                        break;
+                    }
                 
             }
             
         }
+        if(end==-1)
+            end=str.length()-1;
+
+        if((int)(str.charAt(start))==(int)('+')||(int)(str.charAt(start))==(int)('-'))
+            start++;
+        int pow=0;
+        System.out.println("start:"+start+"\nend:"+end);
+        for(int i=end;i>=start;i--)
+        {
+            d+=((int)str.charAt(i)-48)*Math.pow(10,pow);
+            pow++;
+        }
+        if(!positive)
+            d=-d;
+        System.out.println(" "+d);
+        return (int)d;
+
         
     }
  public  static void main(String[] args) {
